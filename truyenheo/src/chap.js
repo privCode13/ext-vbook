ba29c93758ff9f64cab71776068bc64bff7343e9
VietVu <vietvu1398@gmail.com>
2024-12-29 20:26:19 +0700

    Update

--- truyenheo/src/chap.js
+++ truyenheo/src/chap.js
@@ -1,14 +1,19 @@
-load('libs.js');
-load('config.js');
+load("libs.js");
+load("config.js");
 
 function execute(url) {
-    url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL)
-    var response = fetch(url);
-    if (response.ok) {
-        var doc = response.html();
-        var htm = $.Q(doc, '.ndtruyen', { remove: 'em, center, a[target="_blank"]' }).html();
-        htm = htm.replace(/<br>|\\n/g,"<br><br>")
-        return Response.success(htm);
-    }
-    return null;
+  url = url.replace(
+    /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/gim,
+    BASE_URL
+  );
+  var response = fetch(url);
+  if (response.ok) {
+    var doc = response.html();
+    var htm = $.Q(doc, ".ndtruyen", {
+      remove: 'em, a[target="_blank"]',
+    }).html();
+    htm = htm.replace(/<br>|\\n/g, "<br><br>");
+    return Response.success(htm);
+  }
+  return null;
 }
