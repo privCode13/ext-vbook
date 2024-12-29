a8896eadac83404abac5ea15d6d979e897580bb2
privCode13 <dls130198@gmail.com>
2024-12-29 19:08:31 +0700

    Update5 chap.js

--- truyenheo/src/chap.js
+++ truyenheo/src/chap.js
@@ -6,7 +6,7 @@ function execute(url) {
     var response = fetch(url);
     if (response.ok) {
         var doc = response.html();
-        var htm = $.Q(doc, '.ndtruyen', { remove: 'a[target="_blank"]' }).html();
+        var htm = $.Q(doc, '.ndtruyen').html();
         htm = htm.replace(/<br>|\\n/g,"<br><br>")
         return Response.success(htm);
     }
